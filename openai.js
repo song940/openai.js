9b954a95c4865a250a4f25eee5b0acfc9039e5a2
Lsong <song940@gmail.com>
2023-03-04 09:29:49 +0800

    update

--- openai.js
+++ openai.js
@@ -3,16 +3,23 @@ export class OpenAI {
   constructor(configuration) {
     this.configuration = configuration;
   }
-  async createCompletion(request) {
+  async post(path, data) {
     const { api, apiKey } = this.configuration;
-    const res = await fetch(`${api}/v1/completions`, {
-      body: JSON.stringify(request),
+    const res = await fetch(api + path, {
+      body: JSON.stringify(data),
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${apiKey}`
+        "Authorization": `Bearer ${apiKey}`,
       },
     });
     return res.json();
   }
+  createCompletion(request) {
+    return this.post(`/v1/completions`, request);
+  }
+
+  createChatCompletion(request) {
+    return this.post(`/v1/chat/completions`, request);
+  };
 }
